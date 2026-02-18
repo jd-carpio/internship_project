139f9b74352002e2196c524c265677874eb7e99a
jd-carpio <carpiohu@grinnell.edu>
2026-02-18 10:16:13 -0600

    part2

--- src/test/java/com/gamewerks/bgm/WellTests.java
+++ src/test/java/com/gamewerks/bgm/WellTests.java
@@ -1,6 +1,8 @@
 package com.gamewerks.bgm;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import com.gamewerks.bgm.engine.Well;
 
@@ -21,5 +23,66 @@ public class WellTests {
         assertFalse(well.isCompletedRow(1));
         assertFalse(well.isCompletedRow(2));
         assertFalse(well.isCompletedRow(3));
+    }
+
+    @Test
+    public void allTrueWellTest() {
+        boolean[][] grid = {
+          {x, x, x, x},
+          {x, x, x, x},
+          {x, x, x, x},
+          {x, x, x, x}
+        };
+        Well well = new Well(grid);
+        assertTrue(well.isCompletedRow(0));
+        assertTrue(well.isCompletedRow(1));
+        assertTrue(well.isCompletedRow(2));
+        assertTrue(well.isCompletedRow(3));
+    }
+
+    @Test
+    public void MixedWellTest() {
+        boolean[][] grid = {
+          {x, o, x, x},
+          {x, x, o, x},
+          {o, x, x, x},
+          {x, x, x, x}
+        };
+        Well well = new Well(grid);
+        assertFalse(well.isCompletedRow(0));
+        assertFalse(well.isCompletedRow(1));
+        assertFalse(well.isCompletedRow(2));
+        assertTrue(well.isCompletedRow(3));
+    }
+
+    
+    @Test
+    public void DeleteRowTest() {
+        boolean[][] grid = {
+          {x, o, x, x},
+          {x, x, x, x},
+          {x, x, x, x},
+          {x, x, x, x}
+        };
+        Well well = new Well(grid);
+        well.deleteRow(1);
+        assertTrue(well.isCompletedRow(1));
+        assertTrue(well.isCompletedRow(2));
+        assertFalse(well.isCompletedRow(3));
+    }
+
+    @Test
+    public void DeleteRowTest2() {
+        boolean[][] grid = {
+          {o, o, o, o},
+          {x, x, x, x},
+          {x, x, x, x},
+          {x, x, x, x},
+          {o, o, o, o}
+        };
+        Well well = new Well(grid);
+        well.deleteRow(3);
+        assertTrue(well.isCompletedRow(1));
+        assertFalse(well.isCompletedRow(0));
     }
 }
